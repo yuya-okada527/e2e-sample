9b2887a923ba91f18340a202269a278eb13eff52
yuya-okada527 <yuya.okada527@gmail.com>
2021-01-01 00:25:38 +0900

    [Add] スクリーンショットを追加

--- selenium-sample/selenium_sample/yahoo.py
+++ selenium-sample/selenium_sample/yahoo.py
@@ -1,7 +1,9 @@
 """
 Yahoo不動産をSeleniumで操作するサンプル
 """
+import os
 from time import sleep
+from pathlib import Path
 
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
@@ -9,7 +11,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 
+# Yahoo不動産トップURL
 YAHOO_REALESTATE = "https://realestate.yahoo.co.jp/"
+
+# スクリーンショット画像ディレクトリ
+IMAGE_DIR = os.path.join(
+    Path(__file__).resolve().parents[0],
+    "images"
+)
 
 def new_mansion():
     """
@@ -22,6 +31,7 @@ def new_mansion():
         
         # Yahoo!不動産に遷移
         driver.get(YAHOO_REALESTATE)
+        screenshot(driver, "yahoo_realestate_top.png")
 
         # 新築マンションの検索画面に遷移
         new_mansion_link = driver.find_element_by_css_selector(".maNew a")
@@ -44,9 +54,21 @@ def new_mansion():
 
         sleep(3)
 
+        screenshot(driver, "yahoo_realestate_after_search.png")
+
         driver.quit()
     print("新築ブラウズ終了")
-    
+
+
+def screenshot(driver, file_name):
+    # 画面のサイズを設定
+    w = driver.execute_script("return document.body.scrollWidth")
+    h = driver.execute_script("return document.body.scrollHeight")
+    driver.set_window_size(w, h)
+
+    # スクリーンショットを保存
+    driver.save_screenshot(os.path.join(IMAGE_DIR, file_name))
+
 
 def main():
     new_mansion()
