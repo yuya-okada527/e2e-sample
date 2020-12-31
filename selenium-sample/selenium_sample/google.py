d7c795e865e8022576498b5ffacb0abac461b172
yuya-okada527 <yuya.okada527@gmail.com>
2020-12-31 21:42:12 +0900

    [Modify] add docstring

--- selenium-sample/selenium_sample/google.py
+++ selenium-sample/selenium_sample/google.py
@@ -1,13 +1,40 @@
+"""
+GoogleのWebサイトを使って、Seleniumを動かすサンプル
+"""
+
 import time
 
-from selenium.webdriver import Chrome
+from selenium.webdriver import Chrome, ChromeOptions
 
-with Chrome() as driver:
-    driver.get('https://www.google.com/')
-    time.sleep(5)
-    search_box = driver.find_element_by_name("q")
-    search_box.send_keys('ChromeDriver')
-    search_box.submit()
-    time.sleep(5)
-    driver.quit()
 
+def search_chromedriver():
+    """
+    Googleにアクセスし、'ChromeDriver'というクエリで検索を実行するサンプル.
+    """
+    with Chrome() as driver:
+        print("Googleへアクセス中...")
+        driver.get('https://www.google.com/')
+        time.sleep(2)
+        print("検索実行...")
+        search_box = driver.find_element_by_name("q")
+        search_box.send_keys('ChromeDriver')
+        time.sleep(2)
+        search_box.submit()
+        time.sleep(2)
+        driver.quit()
+        print("終了しました.")
+
+
+def exec_headless_mode():
+    """
+    headlessモードで動かすサンプル
+    """
+    pass
+
+
+def main():
+    search_chromedriver()
+
+
+if __name__ == "__main__":
+    main()
