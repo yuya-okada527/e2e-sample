4af91b912fbec7fd814ce71e862b0966326d16fe
yuya-okada527 <yuya.okada527@gmail.com>
2021-01-01 15:14:57 +0900

    [Add] screenshotサンプルをpuppeteerに追加

--- puppeteer-sample/src/node.js
+++ puppeteer-sample/src/node.js
@@ -9,6 +9,8 @@ const args = ["--start-fullscreen", "--disable-infobars", "--hide-scrollbars"];
 
 const google_url = "https://www.google.com/";
 
+const screenshot_dir = "images/";
+
 (async () => {
   // ブラウザを起動
   const browser = await puppeteer.launch({ headless, slowMo, dumpio, args });
@@ -17,6 +19,7 @@ const google_url = "https://www.google.com/";
 
   // Googleに遷移
   await page.goto(google_url);
+  await save(page, "google_top.png");
 
   // 検索実行
   await page.type("input[name='q']", "puppeteer");
@@ -25,6 +28,14 @@ const google_url = "https://www.google.com/";
     timeout: 60000,
     waitUntil: "domcontentloaded",
   });
+  await save(page, "searche_puppeteer.png");
 
   browser.close();
 })();
+
+const save = async (page, fileName) => {
+  await page.screenshot({
+    path: screenshot_dir + fileName,
+    fullPage: true,
+  });
+};
