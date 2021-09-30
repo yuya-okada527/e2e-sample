c077e303023f9b03950f2f3e8cf3180e6c8b489d
yuya-okada527 <yuya.okada527@gmail.com>
2021-09-30 18:24:25 +0900

    change scenario

--- playwright-sample/src/google.spec.ts
+++ playwright-sample/src/google.spec.ts
@@ -2,17 +2,17 @@ const { test, expect } = require("@playwright/test");
 test("test", async ({ page }) => {
   // Googleのトップ画面に遷移
   await page.goto("https://www.google.com/");
-  // 検索ボックスにPlaywrightと入力
-  await page.fill('[aria-label="検索"]', "playwright");
+  // 検索欄に「エクサ」と入力
+  await page.fill('[aria-label="検索"]', "エクサ");
   // エンターキーを押下
   await Promise.all([
     page.waitForNavigation(),
     page.press('[aria-label="検索"]', "Enter"),
   ]);
-  // PlaywrightのGithubリポジトリへのリンクをクリック
-  await page.click("text=microsoft/playwright: Node.js library");
-  // 想定通りのURLに遷移していることを確認
-  await expect(page).toHaveURL("https://github.com/microsoft/playwright");
-  // すぐ閉じると、わかりにくいので、10秒ほど、待機する
+  // エクサのホームページリンクをクリック
+  await page.click("text=株式会社エクサ");
+  // ちゃんとリンクに遷移できているか確認
+  await expect(page).toHaveURL("https://www.exa-corp.co.jp/");
+  // 実行後、わかりやすいように10秒ほど待機
   await new Promise((resolve) => setTimeout(resolve, 10_000));
 });
